5064612f8fdd12075759767f0298935d50fce529
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-07-15 19:42:44 +0300

    adding user repository

---
+++ src/main/kotlin/db/tables/UserTable.kt
@@ -0,0 +1,10 @@
+package db.tables
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+const val MAX_VARCHAR_LENGTH = 50
+
+object UserTable : IntIdTable() {
+    val userName = varchar(name = "userName", length = MAX_VARCHAR_LENGTH).uniqueIndex()
+    val password = varchar(name = "password", length = MAX_VARCHAR_LENGTH)
+}
