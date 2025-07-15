163b0697233422843718b69d95551487188dffd0
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-07-15 12:22:36 +0300

    setting up koin & database

--- src/main/kotlin/db/initDb.kt
+++ src/main/kotlin/db/initDb.kt
@@ -2,11 +2,14 @@ package com.moashraf.db
 
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.initDb() {
+    // All the configuration variables in application.yaml
     val url = environment.config.property("database.url").getString()
     val driver = environment.config.property("database.driver").getString()
     val user = environment.config.property("database.user").getString()
+    // This variable should be added to the environment variables
     val password = System.getenv().get("ktor_moashrafff_db_password")
 
     val db = Database.connect(
@@ -14,4 +17,6 @@ fun Application.initDb() {
         driver = driver,
         user = user
     )
+
+    transaction(db = db) {  }
 }
