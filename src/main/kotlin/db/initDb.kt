b7531401ca3bfbd7a0b29e803d8e0e6cfcd7b43c
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-07-15 14:13:08 +0300

    update db user

--- src/main/kotlin/db/initDb.kt
+++ src/main/kotlin/db/initDb.kt
@@ -2,6 +2,7 @@ package com.moashraf.db
 
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.initDb() {
@@ -18,5 +19,7 @@ fun Application.initDb() {
         user = user
     )
 
-    transaction(db = db) {  }
+    transaction(db = db) {
+
+    }
 }
