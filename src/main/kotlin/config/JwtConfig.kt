add4c75afefff774c5e37cffce11e57afe18ea0f
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-07-15 22:45:33 +0300

    create and verify Jwt

---
+++ src/main/kotlin/config/JwtConfig.kt
@@ -0,0 +1,33 @@
+package com.moashraf.config
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.moashraf.domain.model.User
+import io.ktor.server.application.*
+import java.util.Date
+
+fun Application.generateToken(user: User): String? = try {
+    val jwtSecret = System.getenv("jwt_secret")
+    val jwtIssuer = environment.config.property("jwt.issuer").getString()
+    val jwtAudience = environment.config.property("jwt.issuer").getString()
+    val jwtClaimField = environment.config.property("jwt.issuer").getString()
+    val expTimeInMillis = 24 * 60 * 60 * 1000
+
+    JWT.create().withIssuer(jwtIssuer).withAudience(jwtAudience).withClaim(jwtClaimField, user.id)
+        .withExpiresAt(Date(System.currentTimeMillis() + expTimeInMillis)).sign(Algorithm.HMAC256(jwtSecret))
+
+} catch (e: Exception) {
+    e.printStackTrace()
+    null
+}
+
+fun Application.jwtVerifier(): JWTVerifier {
+    val jwtSecret = System.getenv("jwt_secret")
+    val jwtIssuer = environment.config.property("jwt.issuer").getString()
+    val jwtAudience = environment.config.property("jwt.issuer").getString()
+
+    return JWT.require(
+        Algorithm.HMAC256(jwtSecret)
+    ).withIssuer(jwtIssuer).withAudience(jwtAudience).build()
+}
